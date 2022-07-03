b870963ff84a58c671b84ee5407ec4c84cd33135
Anibal Federico Alvarez <afalvarez@teco.com.ar>
2022-07-03 19:58:09 -0300

    Crear mi primer App con Next y configurar About Page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,29 +1,28 @@
-import type { NextPage } from "next";
 import Head from "next/head";
-
+import { NextPage } from "next/types";
 import styles from "../styles/Home.module.css";
 
-const HomePage: NextPage = () => {
+const AboutPage: NextPage = () => {
     return (
         <div className={styles.container}>
             <Head>
                 <title>App de Fede</title>
-                <meta name="description" content="Home Page" />
+                <meta name="description" content="About Page" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <main className={styles.main}>
                 <h1 className={styles.title}>
-                    Ir al <a href="/about">About Page</a>
+                    Ir al <a href="/">Home</a>
                 </h1>
 
                 <p className={styles.description}>
                     Get started by editing{" "}
-                    <code className={styles.code}>pages/index.tsx</code>
+                    <code className={styles.code}>pages/about.tsx</code>
                 </p>
             </main>
         </div>
     );
 };
 
-export default HomePage;
+export default AboutPage;
